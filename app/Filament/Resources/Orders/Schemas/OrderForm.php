0c1caf5cff09f641b43518c0d5867dbff5e9169d
Yehia Emad <bomacode@gmail.com>
2026-02-12 22:26:08 +0200

    feat: add Filament schema for order form creation and editing.

--- app/Filament/Resources/Orders/Schemas/OrderForm.php
+++ app/Filament/Resources/Orders/Schemas/OrderForm.php
@@ -11,7 +11,7 @@ use App\Models\User;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Grid;
+use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Components\Section;
 use Filament\Forms\Components\Toggle;
 use Filament\Schemas\Components\Utilities\Get;
